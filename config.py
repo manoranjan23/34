3293a084942e1d1c7e4a98b20c37bf2cb032b2ff
SABARISQUAD1984 <selvavinayaga1984@gmail.com>
2024-07-25 18:41:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,17 +11,17 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("6799458147"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/b3d0e737737d67a5bf5a5.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/a19810b838fb13e875d54.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/a19810b838fb13e875d54.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/sastatony")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/team_ligit")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ITZZSABARISQUAD")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ITZlovetime")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6953174731").split()))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
